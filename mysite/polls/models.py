b9f9ad902c72e6512e17475434d0dbea0ed48f18
M Lewis <meli.lewis@gmail.com>
2018-04-02 13:40:36 -0700

    Complete part 7

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -15,8 +15,10 @@ class Question(models.Model):
 
     def was_published_recently(self):
         now = timezone.now()
-        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
